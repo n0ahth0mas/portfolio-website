ce7e60965855a5c29162fca56ecb4d02c321a22e
Noah <npthomas@pugetsound.edu>
2020-04-06 11:54:21 -0700

    Continued structuring homepage

--- js/home.js
+++ js/home.js
@@ -1,17 +1,13 @@
 // JavaScript Document
 
-var sample= [["title1", "caption 1"],["title2", "caption 2"],["title3", "caption 3"],["title4", "caption 4"],["title5", "caption 5"]] ;
+var content= [["The Uncanny Celebrity: Exploring the Implications of Virtual Influencers on Instagram", "This project, created for my Online Communication course, explores the, , , ", "content/digital_influencers/img.jpeg"],["The Uncanny Celebrity: Exploring the Implications of Virtual Influencers on Instagram", "This project, created for my Online Communication course, explores the, , , ", "content/digital_influencers/img.jpeg"],["The Uncanny Celebrity: Exploring the Implications of Virtual Influencers on Instagram", "This project, created for my Online Communication course, explores the, , , ", "content/digital_influencers/img.jpeg"],["The Uncanny Celebrity: Exploring the Implications of Virtual Influencers on Instagram", "This project, created for my Online Communication course, explores the, , , ", "content/digital_influencers/img.jpeg"]] ;
 var portfolio = document.getElementById("portfolioGrid");
-
-var title = "TITLE"
-var caption = "this is a really cool caption!"
-
-var block = '<div class="col-lg-6"><a class="portfolio-item" href="#"><span class="caption"><span class="caption-content"><h2>${title}</h2><p class="mb-0">${caption}</p> </span></span><img class="img-fluid" src="img/portfolio-1.jpg" alt=""></a></div>';
 
 function populatePage(sample){
 	for(var i=0; i<sample.length; i++){
-		portfolio.innerHTML += '<div class="col-lg-6"><a class="portfolio-item" href="#"><span class="caption"><span class="caption-content"><h2>' + sample[i][0] + '</h2><p class="mb-0">' + sample[i][1] +'</p> </span></span><img class="img-fluid" src="img/portfolio-1.jpg" alt=""></a></div>';
+		portfolio.innerHTML += '<div class="col-lg-6"><a class="portfolio-item" href="#"><span class="caption"><span class="caption-content"><p class="mb-0 centered">' + sample[i][0] +'</p> </span></span><img class="img-fluid" src="'+ sample[i][2]+ '" alt=""></a></div>';
 	}
 }
 
-window.onload = populatePage(sample);
+window.onload = populatePage(content
+						);
